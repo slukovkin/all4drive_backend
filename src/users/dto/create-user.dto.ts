cba52ab6cd226f8b5d9e7d472bc3476afdb5a20e
Sergey Lukovkin <slukovkin@icloud.com>
2023-06-29 19:50:14 +0300

    added validation in users module

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,12 +1,18 @@
 import { ApiProperty } from '@nestjs/swagger';
+import {IsEmail, IsNotEmpty, Length} from "class-validator";
 
 export class CreateUserDto {
   @ApiProperty({ example: 'user@gmail.com', description: 'Email пользователя' })
+  @IsEmail()
+  @IsNotEmpty()
   readonly email: string;
 
   @ApiProperty({ example: '123456', description: 'Пароль пользователя' })
+  @IsNotEmpty()
+  @Length(4, 10)
   readonly password: string;
 
   @ApiProperty({ example: 'stock', description: 'Склад пользователя' })
+  @IsNotEmpty()
   readonly store?: string;
 }
