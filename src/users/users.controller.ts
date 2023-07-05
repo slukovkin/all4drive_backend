49b1c8e10a76ec2f72546db38a2622ee6618e4a4
Sergey Lukovkin <slukovkin@icloud.com>
2023-07-05 20:55:45 +0300

    added cors and disable guards

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,35 +1,36 @@
-import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
-import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UsersService } from './users.service';
-import { User } from './users.model';
-import { Role, Roles } from '../roles/roles.decorator';
-import { AuthGuard } from '../guards/auth.guard';
+import {Body, Controller, Get, Param, Post, UseGuards} from '@nestjs/common';
+import {ApiOperation, ApiResponse, ApiTags} from '@nestjs/swagger';
+import {CreateUserDto} from './dto/create-user.dto';
+import {UsersService} from './users.service';
+import {User} from './users.model';
+import {Role, Roles} from '../roles/roles.decorator';
+import {AuthGuard} from '../guards/auth.guard';
 
 @ApiTags('Пользователи')
 @Controller('users')
 export class UsersController {
-  constructor(private readonly userService: UsersService) {}
+  constructor(private readonly userService: UsersService) {
+  }
 
-  @ApiOperation({ summary: 'Создание пользователя' })
-  @ApiResponse({ status: 200, type: User })
+  @ApiOperation({summary: 'Создание пользователя'})
+  @ApiResponse({status: 200, type: User})
   @Post()
-  @Roles(Role.Admin)
+  // @Roles(Role.Admin)
   create(@Body() userDto: CreateUserDto) {
     return this.userService.createUser(userDto);
   }
 
-  @ApiOperation({ summary: 'Получение всех пользователей' })
-  @ApiResponse({ status: 200, type: [User] })
-  @UseGuards(AuthGuard)
+  @ApiOperation({summary: 'Получение всех пользователей'})
+  @ApiResponse({status: 200, type: [User]})
+  // @UseGuards(AuthGuard)
   @Get()
   getAllUsers() {
     return this.userService.getAllUsers();
   }
 
-  @ApiOperation({ summary: 'Получение пользователя по ID' })
-  @ApiResponse({ status: 200, type: User })
-  @UseGuards(AuthGuard)
+  @ApiOperation({summary: 'Получение пользователя по ID'})
+  @ApiResponse({status: 200, type: User})
+  // @UseGuards(AuthGuard)
   @Get(':id')
   getOneUser(@Param('id') id: string) {
     return this.userService.getOneUser(id);
